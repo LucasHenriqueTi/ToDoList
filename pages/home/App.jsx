5088cf73edebbc0f23e8aba4b8ac15eeb6b28d50
lucas <lucas.henrique.andrade.ti@gmail.com>
2024-10-21 20:10:55 -0300

    segunda atualização

--- pages/home/App.jsx
+++ pages/home/App.jsx
@@ -1,48 +1,52 @@
 import './App.css'
-import Lixeira from '../../src/assets/lixo.svg'
+import Lixeira from '../../src/assets/lixo.svg';
+import { usuarios } from '../../server/dados.js'
+
 
 function Formulario() {
   return (
-    <div className='container'>
+    <div className='form'>
       <form >
-        <h2>ToDo List</h2>
+        <h2>Cadastro de Clientes</h2>
         <input type="text" name="nome" id="nome" placeholder='Nome' /><br />
-        <input type="number" name="telefone" id="numero" placeholder='Telefone' /> <br />
+        <input type="number" name="idade" id="numero" placeholder='Idade' /> <br />
         <input type="email" name="e-mail" id="email" placeholder='E-mail' /><br />
-      </form>
         <button type='button'>enviar</button>
+      </form>
     </div>
   );
 }
 
 function Listausuarios() {
   return (
-    <div>
-        <div>
-        <p>Nome: </p>
-        <p>Telefone: </p>
-        <p>Email: </p>
-      </div>
-      <button className='lixeira'>
-        <img src={Lixeira} alt="lixeira" />
-      </button>
-    </div>
-    
+    <>
+      {usuarios.map(user =>
+        <div className='user'>
+          <div>
+            <p>Nome:<span>{user.name} </span> </p>
+            <p>Idade:<span>{user.idade}</span> </p>
+            <p>Email:<span>{user.email}</span> </p>
+          </div>
+          <button className='lixeira'>
+            <img src={Lixeira} alt="lixeira" />
+          </button>
+        </div>)}
+    </>
   );
 }
 
 function App() {
 
   return (
-    <>
+    <div className='container'>
       <div>
         <Formulario />
       </div>
       <div>
-        <Listausuarios/>
+        <Listausuarios />
       </div>
-    </>
-    
+    </div>
+
   );
 }
 
---
+++ server/dados.js
@@ -0,0 +1,12 @@
+export const usuarios = [{
+    id:1,
+    name:'lucas',
+    idade: 22,
+    email: 'lucas@email.com'
+},
+{
+    id:2,
+    name:'andre',
+    idade: 25,
+    email: 'andre@email.com'
+}];
